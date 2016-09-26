367283d564ce4418d0797013099007a0728e922c
Andres Galaviz <andres.galaviz@gmail.com>
2016-09-25 23:29:31 -0400

    Initial commit

---
+++ RiperLex.py
@@ -0,0 +1,82 @@
+import ply.lex as lex
+
+# Reserved words
+reserved = {
+    'main' : 'MAIN',
+    'function' : 'FUNCTION',
+    'void' : 'VOID',
+    'int' : 'INTTYPE',
+    'float' : 'FLOATTYPE',
+    'string' : 'STRINGTYPE',
+    'bool' : 'BOOLTYPE',
+    'array' : 'ARRAY',
+    'console' : 'CONSOLE',
+    'input' : 'INPUT',
+    'if' : 'IF',
+    'elif' : 'ELIF',
+    'else' : 'ELSE',
+    'for' : 'FOR',
+    'while' : 'WHILE',
+    'do' : 'DO',
+    'return' : 'RETURN',
+    'true' : 'TRUE',
+    'false' : 'FALSE'
+}
+
+# List of tokens
+tokens = ['ID',
+          'AND',
+          'OR',
+          'LESS',
+          'GREATER',
+          'DIFFERENT',
+          'EQUALTO',
+          'LESSEQUAL',
+          'GREATEREQUAL',
+          'FLOAT',
+          'INT',
+          'STRING'] + list(reserved.values())
+
+literals = ['=', '+', '-', '*', '/', '{', '}', '(', ')', '[', ']', ':', ',', ';', '%']
+
+# Rules for tokens
+t_ignore = ' \t\n'
+t_EQUALTO = r'=='
+t_LESS = r'<'
+t_GREATER = r'>'
+t_DIFFERENT = r'!='
+t_LESSEQUAL = r'<='
+t_GREATEREQUAL = r'>='
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_FLOAT = r'[0-9]+\.[0-9]+'
+t_INT = r'[0-9]+'
+t_STRING = r'\".*\"'
+
+# Lookup in case of reserved words
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Error handling rule
+# def t_error(t):
+#     print("Lexer error %s" % t.value[0])
+#     exit(-1)
+#     t.lexer.skip(1)
+
+def t_error(t):
+    t.type = t.value[0]
+    t.value = t.value[0]
+    t.lexer.skip(1)
+    print("Illegal character '%s'" % t.value[0])
+    print(t.lexer.lineno)
+    exit(-1)
+
+# Build the lexer
+lexer = lex.lex()
---
+++ RiperPar.py
@@ -0,0 +1,272 @@
+#To compile
+#python RiperLex.py 
+#python RiperPar.py test.riper
+
+import ply.yacc as yacc
+import sys
+import RiperLex
+# import the lexical tokens
+tokens = RiperLex.tokens
+
+# define grammar rules
+def p_program(p):
+  '''program : initVarDeclar functionDeclar main'''
+  p[0] = 'OK'
+  
+
+def p_initVarDeclar(p):
+  '''initVarDeclar : varDeclar initVarDeclar
+                   | '''
+def p_functionDeclar(p):
+  '''functionDeclar : function functionDeclar
+                    | '''
+
+def p_varDeclar(p):
+  '''varDeclar : vars ';'
+    | ARRAY arrays ';' '''
+  
+
+def p_vars(p):
+  '''vars : type ID '=' expression moreVar'''
+
+def p_moreVar(p):
+  '''moreVar : ',' ID '=' expression moreVar
+    | '''
+  
+
+def p_type(p):
+  '''type : INTTYPE
+    | FLOATTYPE
+    | STRINGTYPE
+    | BOOLTYPE '''
+  
+
+def p_arrays(p):
+  '''arrays : type ID '[' constant ']' '=' '{' expression moreExp '}' moreArray '''
+  
+
+def p_moreExp(p):
+  '''moreExp : ',' expression moreExp
+    | '''
+  
+
+def p_moreArray(p):
+  '''moreArray : ',' ID '[' constant ']' '=' '{' expression moreExp '}' moreArray
+    | '''
+  
+
+def p_function(p):
+  '''function : FUNCTION funcType ID '(' par ')' '{' block RETURN returnType ';' '}' '''
+  
+
+def p_funcType(p):
+  '''funcType : type
+    | VOID '''
+  
+
+def p_returnType(p):
+  '''returnType : expression
+    | VOID '''
+  
+
+def p_main(p):
+  '''main : FUNCTION MAIN '(' par ')' '{' block '}' '''
+  
+
+def p_par(p):
+  '''par : type ID morePar
+         | '''
+  
+
+def p_morePar(p):
+  '''morePar : ',' type ID morePar
+    | '''
+  
+
+def p_funcCall(p):
+  '''funcCall : ID '(' parIn ')' '''
+  
+
+def p_parIn(p):
+  '''parIn : expression moreParIn
+    | '''
+  
+
+def p_moreParIn(p):
+  '''moreParIn : ',' expression moreParIn 
+     | '''
+  
+
+def p_block(p):
+  '''block : varDeclar block
+            | assign block
+            | conditional block
+            | loop block
+            | funcCall block
+            | output block
+            | input block
+            | '''
+  
+
+def p_loopBlock(p):
+  '''loopBlock : assign loopBlock
+    | conditional loopBlock
+    | loop loopBlock
+    | funcCall loopBlock
+    | output loopBlock
+    | input loopBlock
+    | '''
+  
+
+def p_assign(p):
+  '''assign : ID possibleArray '=' expression ';' '''
+
+def p_possibleArray(p):
+  '''possibleArray : '[' exp ']'
+    | '''
+  
+
+def p_conditional(p):
+  '''conditional : IF '(' expression ')' '{' block '}' possibleElif possibleElse '''
+  
+
+def p_possibleElif(p):
+  '''possibleElif : ELIF '(' expression ')' '{' block '}' possibleElif
+    | '''
+  
+
+def p_possibleElse(p):
+  '''possibleElse : ELSE '{' block '}' '''
+  
+
+def p_output(p):
+  '''output : CONSOLE '(' expression ')' ';' '''
+  
+
+def p_loop(p):
+  '''loop : for
+    | while
+    | doWhile '''
+  
+
+def p_for(p):
+  '''for : FOR '(' expression ')' '{' loopBlock '}' '''
+  
+
+def p_while(p):
+  '''while : WHILE '(' expression ')' '{' loopBlock '}' '''
+  
+
+def p_doWhile(p):
+  '''doWhile : DO '{' loopBlock '}' while '(' expression ')' ';' '''
+  
+
+def p_expression(p):
+  '''expression : higherExp possibleHigherExp'''
+  
+
+def p_possibleHigherExp(p):
+  '''possibleHigherExp : possibleHigherExpOp higherExp
+    | '''
+  
+
+def p_possibleHigherExpOp(p):
+  '''possibleHigherExpOp : AND
+    | OR '''
+  
+
+def p_higherExp(p):
+  '''higherExp : exp possibleExp'''
+  
+
+def p_possibleExp(p):
+  '''possibleExp : possibleExpOp exp
+    | '''
+  
+
+def p_possibleExpOp(p):
+  '''possibleExpOp : LESS
+    | GREATER
+    | LESSEQUAL
+    | GREATEREQUAL
+    | DIFFERENT
+    | EQUALTO '''
+  
+
+def p_exp(p):
+  '''exp : term possibleTerms'''
+  
+
+def p_possibleTerms(p):
+  '''possibleTerms : possibleTermOp term possibleTerms
+    | '''
+  
+
+def p_possibleTermOp(p):
+  '''possibleTermOp : '+'
+  | '-' '''
+  
+
+def p_term(p):
+  '''term : factor possibleFactors'''
+  
+
+def p_possibleFactors(p):
+  '''possibleFactors : possibleFactorOp factor possibleFactors
+    | '''
+  
+
+def p_possibleFactorOp(p):
+  '''possibleFactorOp : '*'
+    | '/'
+    | '%' '''
+  
+
+def p_factor(p):
+  '''factor : '(' expression ')'
+    | data'''
+  
+
+def p_data(p):
+  '''data : ID possibleIdCall
+    | constant'''
+  
+
+def p_possibleIdCall(p):
+  '''possibleIdCall : '[' expression ']'
+    | '(' parIn ')'
+    | '''
+  
+
+def p_constant(p):
+  '''constant : INT
+    | FLOAT
+    | bool
+    | STRING'''
+
+def p_bool(p):
+  '''bool : TRUE 
+          | FALSE'''
+
+def p_input(p):
+  '''input : INPUT '(' STRING ')' ';' '''
+  
+
+def p_error(p):
+      print('Syntax error in line %d token %s with value %s' % (p.lineno, p.type, p.value))
+
+  # Build the parser
+RiperParser = yacc.yacc()
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        file = sys.argv[1]
+        try:
+            f = open(file,'r')
+            data = f.read()
+            f.close()
+            if (RiperParser.parse(data, tracking=True) == 'OK'):
+                print ('This is a correct and complete Riper program');
+        except EOFError:
+            print(EOFError)
+    else:
+        print('File missing')
